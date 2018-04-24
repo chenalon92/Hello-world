fbcc15a53565b653d27be707ac2327fff9e758ef
chen alon <chen.alon@fiverr.com>
2018-04-24 16:24:26 +0300

    hello world, goodbye world

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def hello
+  	render html: "hello, world!"
+  end
+  def hola
+  	render html: "¡hola, mundo!"
+  end
+  def goodbye
+  	render html: "goodbye, world!"
+  end
+end
